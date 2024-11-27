817f4f3744da2d5eb6daa5c5b4e7ecf9d5f5fa3e
penny <penny0723@naver.com>
2024-11-27 21:47:25 +0900

    ui: 글로벌 에러 페이지 CSS

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Button from './ui/Button';
+
 export default function GlobalError({
   error,
   reset,
@@ -11,9 +13,20 @@ export default function GlobalError({
     <html>
       <body>
         <section className="m-auto max-w-screen-2xl">
-          <div className="flex items-center justify-center">
-            <h2>Something went wrong!</h2>
-            <button onClick={() => reset()}>Try again</button>
+          <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+            <div className="mx-auto flex max-w-screen-sm flex-col items-center justify-center text-center">
+              <p className="mb-4 text-xl font-bold tracking-tight text-gray-900 md:text-4xl dark:text-white">
+                서버의 문제로 페이지를 불러오지 못했습니다.
+              </p>
+              <div className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+                다시 시도하시겠습니까?
+              </div>
+              <div className="my-4 flex w-24 items-center justify-center">
+                <Button onClick={() => reset()} variant="primary" size="md">
+                  다시 시도
+                </Button>
+              </div>
+            </div>
           </div>
         </section>
       </body>
