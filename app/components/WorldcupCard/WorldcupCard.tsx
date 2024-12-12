bf3d688e84e4dffe20e158be26c09b191607e364
penny <yjo0723@hotmail.com>
2024-12-12 20:06:43 +0900

    ui: 카드 제목 크기

--- app/components/WorldcupCard/WorldcupCard.tsx
+++ app/components/WorldcupCard/WorldcupCard.tsx
@@ -29,7 +29,7 @@ export default function WorldcupCard({ worldcupCard, className, type = 'default'
             <div className="flex-1">
               <Link href={`/wc/${id}`}>
                 <h2
-                  className="mb-0.5 line-clamp-2 cursor-pointer text-base font-bold text-slate-700 hover:underline"
+                  className="mb-0.5 line-clamp-2 cursor-pointer text-lg font-semibold text-slate-700 hover:underline"
                   title={title}
                 >
                   {title}
